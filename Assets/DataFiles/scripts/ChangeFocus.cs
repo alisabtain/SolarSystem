ae62ee2e9500734efce502363fe4b6c12b177c2b
alisabtain <msabtain@monmouthcollege.edu>
2021-06-17 10:38:00 -0500

    add script LookAt/ChangeFocus to focus on celestial bodies during gameplay by clicking on them

---
+++ Assets/DataFiles/scripts/ChangeFocus.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChangeFocus : MonoBehaviour
+{
+    public GameObject target;
+    
+
+    void OnMouseDown()
+    {
+
+        LookAt.target = target;
+        Camera.main.fieldOfView = Mathf.Clamp(60 * target.transform.localScale.x, 1, 100);
+
+
+
+    }
+}
